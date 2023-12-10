ffcd29c7aed800be44badddc59761c5426019c08
Raju Kumar <129931026+Rajusudha@users.noreply.github.com>
2023-12-10 14:55:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
+  if (!arr || n <= 0) {
+    return "Invalid input";
+  }
+
+  const result = [];
+  let currentSubarray = [];
+
+  let currentSum = 0;
+  for (const num of arr) {
+    if (currentSum + num <= n) {
+      currentSubarray.push(num);
+      currentSum += num;
+    } else {
+      result.push([...currentSubarray]);
+      currentSubarray = [num];
+      currentSum = num;
+    }
+  }
+
+  // Adding the last subarray
+  result.push([...currentSubarray]);
+
+  return result;
 };
 
-const n = prompt("Enter n: ");
-alert(JSON.stringify(divide(arr, n)));
+const userN = prompt("Enter n: ");
+const parsedN = parseInt(userN);
+
+if (!isNaN(parsedN)) {
+  alert(JSON.stringify(divide(arr, parsedN)));
+} else {
+  alert("Please enter a valid number for n.");
+}
+
